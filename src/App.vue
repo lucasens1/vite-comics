24d951379657fb187c522f1f95550dd971393959
lucasens1 <lucasensini1@gmail.com>
2024-05-02 16:32:23 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<script>
+  import AppHeader from './components/AppHeader.vue';
+  import AppMain from './components/AppMain.vue';
+  import AppFooter from './components/AppFooter.vue';
+
+  export default {
+    components: {
+      AppHeader,
+      AppMain,
+      AppFooter
+    }
+  }
+</script>
+
+<template>
+
+  <!-- Header -->
+  <AppHeader />
+  <!-- Main -->
+  <AppMain />
+  <!-- Footer -->
+  <AppFooter />
+
+</template>
+
+<style>
+</style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,68 @@
+<script>
+export default {
+    data(){
+        return {
+            //Array di oggetti contenenti titolo della voce nell'header e indicatore Flag della classe active
+            menu_items : [
+                {
+                    title : "Characters",
+                    activeFlag : false
+                },
+                {
+                    title : "Comics",
+                    activeFlag : true
+                },
+                {
+                    title : "Movies",
+                    activeFlag : false
+                },
+                {
+                    title : "TV",
+                    activeFlag : false
+                },
+                {
+                    title : "Games",
+                    activeFlag : false
+                },
+                {
+                    title : "Collectibles",
+                    activeFlag : false
+                },
+                {
+                    title : "Videos",
+                    activeFlag : false
+                },
+                {
+                    title : "Fans",
+                    activeFlag : false
+                },
+                {
+                    title : "News",
+                    activeFlag : false
+                },
+                {
+                    title : "Shop",
+                    activeFlag : false
+                },
+            ],
+        };
+    },
+};
+</script>
+
+<template>
+    <header>
+      <div class="header-logo">
+        <img
+          src="../assets/img/dc-logo.png"
+          alt="Logo DC"
+        />
+      </div>
+      <ul>
+        <li v-for = "item in menu_items">{{item.title}}</li>
+      </ul>
+    </header>
+  </template>
+
+<style lang="scss" scoped>
+</style>
