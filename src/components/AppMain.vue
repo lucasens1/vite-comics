c1a6c654c6ec946c6d6ff1796d7427ed0ac0bbdc
lucasens1 <lucasensini1@gmail.com>
2024-05-03 17:05:58 +0200

    minor fixes

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,4 +1,19 @@
 <script>
+//Importo Array e Card
+import { arrayDC } from "../assets/data/comicsArray.js"; 
+import AppCard from "./AppCard.vue"; 
+
+export default {
+  components: {
+    AppCard,
+  },
+  data() {
+    return {
+      //Assegno l'array a comics
+      comics : arrayDC,
+    };
+  },
+};
 </script>
 
 <template>
@@ -7,14 +22,27 @@
             <img src="../assets/img/jumbotron.jpg" alt="">
         </div>
     </div>
-    <div class="cards w-80">
-
-    </div>
+    <section class="comics-section">
+        <div class="row">
+          <div class="col" v-for="comics in arrayDC">
+            <AppCard
+              :img ="comics.thumb"
+              :name ="comics.series"
+              :altext ="comics.type"
+            />
+          </div>
+        </div>
+      </section>
 </template>
 
 <style lang = "scss" scoped>
+/* Importo flex */
+@use "../style/partials/_mixins" as *;
 /* Importo le classi dal file generals.scss */
 @import '../style/generals.scss';
+
+
+
 .bg-black {
     background-color : black;
     width : 100%;
@@ -28,5 +56,22 @@
         width: 100%;
         object-fit: fill;
     }
+}
+
+
+.comics-section {
+  margin-top: 2rem;
+  padding: 3rem 3rem;
+
+  @include flex(column, center, center);
+
+  .row {
+    @include flex(row, space-between, stretch);
+    flex-wrap: wrap;
+    .col {
+      width: 30%;
+      margin-bottom: 1.5rem;
+    }
+  }
 }
 </style>
